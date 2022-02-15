4d80e946d95442fa58b44c5e360ea2de54b5bdd9
Vamsi Krishna Gali <gali.krishna97@gmail.com>
2022-02-15 17:46:44 -0500

    Added README File

--- Node.java
+++
@@ -1,9 +0,0 @@
-//A class to store nodes in the graph
-class Node {
-    int vertex, weight;
-    //Parameterised Constructor
-    public Node(int vertex, int weight) {
-        this.vertex = vertex;
-        this.weight = weight;
-    }
-}
